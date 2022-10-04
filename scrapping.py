c6f2cdd6017cbcbe0b003ad7722c7c2bcecea004
Caio Henrik <58925106+Caiohh@users.noreply.github.com>
2022-10-04 18:01:56 -0300

    Update scrapping.py

--- scrapping.py
+++ scrapping.py
@@ -1,5 +1,16 @@
-import requests
 from bs4 import BeautifulSoup
-import re
-import pandas as pd
-import math
+import requests
+
+# Site que será coletado
+site = "http://www.pudim.com.br/"
+
+# Coleta de dados do site
+html = requests.get(site).content
+dados = BeautifulSoup(html, 'html.parser')
+email = dados.find("div", class_="email")
+
+# coletando a tag dentro da div email
+link = email.find("a")
+print("1", link)  
+print("2", link.text)
+print("3", link.attrs['href'])
